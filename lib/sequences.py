2d8203157afcec673ea9bb349061fd56624095ef
Stephen Mbau <mbaustephen@gmail.com>
2023-10-04 10:44:49 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print("[]")
+    elif length == 1:
+        print("[0]")
+    else:
+        a, b = 0, 1
+        fibonacci_sequence = [a, b]
+
+        while len(fibonacci_sequence) < length:
+            next_fibonacci = a + b
+            fibonacci_sequence.append(next_fibonacci)
+            a, b = b, next_fibonacci
+
+        print(fibonacci_sequence)
+    
+
+print_fibonacci(0)  # Prints: []
+print_fibonacci(1)  # Prints: [0]
+print_fibonacci(2)  # Prints: [0, 1]
+print_fibonacci(10)  # Prints the first 10 Fibonacci numbers
